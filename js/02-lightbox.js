898c73115c5ea1338ec9bc7120c3dc7d8e73478d
Aleksandr Yemchenko <aleksandremchenko@gmail.com>
2023-03-05 16:20:07 +0200

    fix task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -13,17 +13,4 @@ const makeElements = galleryItems.map(makeElementOfGallery).join("");
 const galleryEl = document.querySelector(".gallery");
 galleryEl.insertAdjacentHTML("afterbegin", makeElements);
 
-const galleryBox = document.querySelector(".gallery");
-galleryBox.addEventListener("click", handleClickOnImage);
-
-function handleClickOnImage(event) {
-  event.preventDefault();
-
-  if (event.target.nodeName !== "IMG") {
-    return;
-  } else {
-    
-    return new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
-    
-  }
-}
+new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
